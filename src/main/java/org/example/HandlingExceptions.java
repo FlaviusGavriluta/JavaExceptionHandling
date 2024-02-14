99df19059389da01ac449ff310afed0b51859d38
flaviusgavriluta <flavius.gavriluta@gmail.com>
2024-02-14 12:22:55 +0200

    we have this situation with an error occured

--- src/main/java/org/example/HandlingExceptions.java
+++ src/main/java/org/example/HandlingExceptions.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class HandlingExceptions {
     public static void main(String[] args) {
+        double[] prices = {5.90, 18.99, 64.0, 99.00, 79.50};
+        int index = 5;
 
+        System.out.println("Start of the program");
+        System.out.println(String.format("\nThe price at index %d is %f\n", index, prices[index]));
+        System.out.println("End of the program");
     }
 }
