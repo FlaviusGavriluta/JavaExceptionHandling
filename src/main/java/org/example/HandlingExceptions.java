bb632c4d3c50cc1962c4364dde355ed2d69e2bec
flaviusgavriluta <flavius.gavriluta@gmail.com>
2024-02-14 14:40:47 +0200

    another type to handle an exception with null pointer

--- src/main/java/org/example/HandlingExceptions.java
+++ src/main/java/org/example/HandlingExceptions.java
@@ -6,17 +6,13 @@ public class HandlingExceptions {
         int index = 5;
 
         System.out.println("Start of the program");
+
         try {
             System.out.println(String.format("\nThe price at index %d is %f\n", index, prices[index]));
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("An Exception occurred:");
-            System.out.println("You have exceeded the array bounds.");
-
-            if (index < 0)
-                System.out.println("The first price in the array is: " + prices[0]);
-            else
-                System.out.println("The last price in the array is: " + prices[prices.length - 1]);
+        } catch (NullPointerException npe) {
+            System.out.println("We never get here - the wrong Exception has been caught");
         }
+
         System.out.println("End of the program");
     }
 }
