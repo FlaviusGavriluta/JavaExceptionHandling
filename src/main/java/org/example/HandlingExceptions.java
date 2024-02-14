01cb41f6bf4144473cc81f86d8c79080c59b526e
flaviusgavriluta <flavius.gavriluta@gmail.com>
2024-02-14 14:34:50 +0200

    handling catch block

--- src/main/java/org/example/HandlingExceptions.java
+++ src/main/java/org/example/HandlingExceptions.java
@@ -8,15 +8,14 @@ public class HandlingExceptions {
         System.out.println("Start of the program");
         try {
             System.out.println(String.format("\nThe price at index %d is %f\n", index, prices[index]));
-        } catch (Exception e) {
-            System.out.println("An Exception occurred");
-            System.out.println("Is e an Object? " + (e instanceof Object));
-            System.out.println("Is e a Throwable? " + (e instanceof Throwable));
-            System.out.println("Is e an Exception? " + (e instanceof Exception));
-            System.out.println("Is e a RuntimeException? " + (e instanceof RuntimeException));
-            System.out.println("Is e a ArrayIndexOutOfBoundsException? " + (e instanceof ArrayIndexOutOfBoundsException));
-            System.out.println("Is e a ArithmeticException? " + (e instanceof ArithmeticException));
-            System.out.println("Is e a StringIndexOutOfBoundsException? " + (e instanceof StringIndexOutOfBoundsException));
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("An Exception occurred:");
+            System.out.println("You have exceeded the array bounds.");
+
+            if (index < 0)
+                System.out.println("The first price in the array is: " + prices[0]);
+            else
+                System.out.println("The last price in the array is: " + prices[prices.length - 1]);
         }
         System.out.println("End of the program");
     }
